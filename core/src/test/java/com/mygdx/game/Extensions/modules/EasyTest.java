8c423ec5c4891825a5673e1f1c04d7ecb9a96ed3
Roman <bolobolbfmv@gmail.com>
2018-06-01 01:10:28 +0300

    try add test

---
+++ core/src/test/java/com/mygdx/game/Extensions/modules/EasyTest.java
@@ -0,0 +1,12 @@
+package com.mygdx.game.Extensions.modules;
+
+import static org.junit.Assert.*;
+
+public class EasyTest {
+    @org.junit.Test
+    public void run() throws Exception {
+
+        fail("The test case is a prototype.");
+    }
+
+}
