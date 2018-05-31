21a25aaa600fcfd6f22ccd3721e1f34fc66d5c97
Roman <bolobolbfmv@gmail.com>
2018-06-01 01:16:05 +0300

    add test

--- core/src/test/java/com/mygdx/game/Extensions/modules/EasyTest.java
+++ core/src/test/java/com/mygdx/game/Extensions/modules/EasyTest.java
@@ -6,6 +6,27 @@ public class EasyTest {
     @org.junit.Test
     public void run() throws Exception {
 
+        Easy instance = new Easy();
+
+
+
+        boolean result = true;
+
+        double old_x =instance.player.sprite().getOldX();
+        double old_y =instance.player.sprite().getOldY();
+
+        instance.run();
+        if(old_x == instance.player.sprite().getOldX() && old_y == instance.player.sprite().getOldY()){
+
+            result = false;
+
+        }
+
+        assertEquals(true, result);
+
+// TODO review the generated test code and remove the default call to fail.
+
+        fail("The test case is a prototype.");
         fail("The test case is a prototype.");
     }
 
